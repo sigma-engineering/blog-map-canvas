fa370f2b54c962b052aedf045f7c1ca210a7e01f
Ben Wiklund <ben@daisyowl.com>
2020-07-10 12:17:55 -0700

    load

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { times } from "lodash";
 import css from "./App.module.css";
 
 function App() {
@@ -13,6 +12,14 @@ function App() {
 const colorTheme = ["#e63946", "#f1faee", "#a8dadc", "#457b9d", "#1d3557"];
 
 function MapCanvas() {
+  const [data, setData] = React.useState<string[]>();
+  React.useEffect(() => {
+    fetch("./data.txt")
+      .then((d) => d.text())
+      .then((text) => setData(text.split("\n")));
+  }, []);
+
+  console.log(data);
   return <p>Here is where the map will go</p>;
 }
 
